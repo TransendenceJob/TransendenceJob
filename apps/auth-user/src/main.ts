4bb0e692259da70d8286dda0cb7138f71d45e062
onlyHydra <chiperstefantheodor@gmail.com>
2026-03-13 17:31:08 +0100

    [End] [#18] added a small /health endpoint to check filters,middleware,interceptors

--- apps/auth-user/src/main.ts
+++ apps/auth-user/src/main.ts
@@ -1,13 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './modules/common/filters/global-exception.filter';
 
 
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+		}),
+	);
+	app.useGlobalFilters(new GlobalExceptionFilter());
 
-
-  await app.listen(process.env.PORT ?? 3000);
+	app.useGlobalInterceptors(
+		new ClassSerializerInterceptor(app.get(Reflector)),
+	);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+
