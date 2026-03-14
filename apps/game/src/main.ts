3cb8e5b23365ba63216f7289e6767d3a7d83d8cb
Lilly K. Home <likunne@DESKTOP-QBDTI2D.localdomain>
2026-03-14 19:13:57 +0100

    Added basic websocket test

--- apps/game/src/main.ts
+++ apps/game/src/main.ts
@@ -7,5 +7,6 @@ import { ServerModule } from './server.module';
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- apps/game/src/server.module.ts
+++ apps/game/src/server.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServerController } from './server.controller';
 import { LobbyManager } from './lobbies/LobbyManager'
+import { EventsGateway } from './ws/events.gateway'
 
 @Module({
   imports: [],
   controllers: [ServerController],
-  providers: [LobbyManager],
+  providers: [LobbyManager, EventsGateway],
 })
 export class ServerModule {}
 
