455f596a04da71e8211f140090862c5f660ce5fc
tbui-quo <tbui-quo@student.42wolfsburg.de>
2026-03-16 18:22:22 +0100

    [New][#16]

--- apps/frontend/app/homepage/page.tsx
+++ apps/frontend/app/homepage/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// need to add darkmode theme
 export default function HomePage() {
     return (
         <div className="max-w-7xl mx-auto p-4 md:p-8 h-[calc(100vh-80px)] flex flex-col gap-6">
