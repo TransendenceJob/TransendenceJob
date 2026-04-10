d45d23797f294eca52b76c378d3ab95d5e95f249
Mohamad Javad Khavari <mkhavari@c2r9s2.42wolfsburg.de>
2026-04-10 14:38:59 +0200

    [#72] Prisma still in shambles added all the folder structure

--- apps/stats/src/main.ts
+++ apps/stats/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap() : Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('/internal/auth')
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }),
+  );
+  
+  app.enableShutdownHooks()
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- apps/stats/src/modules/player-stats/player-stats.controller.ts
+++ apps/stats/src/modules/player-stats/player-stats.controller.ts
@@ -11,3 +11,4 @@ export class PlayerStatsController {
 		return this.service.createStatsForUser(Number(id));
 	}
 }
+
