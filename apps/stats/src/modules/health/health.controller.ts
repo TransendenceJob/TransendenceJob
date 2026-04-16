7a9229dd35577042e8fe579e4373c194997dc479
Mohamad Javad Khavari <mkhavari@c2r9s2.42wolfsburg.de>
2026-04-16 13:52:44 +0200

    [End] [#72] closed this ticket, I tried to write all methods we need in stats_service as I found we need, A_REQ.http already listed those requests

--- apps/stats/src/modules/health/health.controller.ts
+++ apps/stats/src/modules/health/health.controller.ts
@@ -1,33 +1,35 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, TypeOrmHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import {
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+  HealthCheck,
+} from '@nestjs/terminus';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('health')
 export class HealthController {
-	constructor(
-		private health: HealthCheckService,
-		private prisma: PrismaService
-	) {}
+  constructor(
+    private health: HealthCheckService,
+    private prisma: PrismaService,
+  ) {}
 
-	@Get()
-    checkStatus() {
-        return { status: 'ok' };
-    }
-	
-	@Get('db')
-	@HealthCheck()
-	async check() {
-		return this.health.check([
-			async () => {
-				try {
-					await this.prisma.$queryRaw`SELECT 1`;
-					return { prismadb: { status: 'up' } };
-				} catch (error) {
-					throw new Error('Prisma connection failed');
-				}
-			}
-		]);
-		
-	}
+  @Get()
+  checkStatus() {
+    return { status: 'ok' };
+  }
+
+  @Get('db')
+  @HealthCheck()
+  async check() {
+    return this.health.check([
+      async () => {
+        try {
+          await this.prisma.$queryRaw`SELECT 1`;
+          return { prismadb: { status: 'up' } };
+        } catch (error) {
+          throw new Error('Prisma connection failed');
+        }
+      },
+    ]);
+  }
 }
-
