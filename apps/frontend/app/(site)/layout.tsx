804225b3a6aa7753e030e1f370cb4147b98906fd
Lilly K. Home <likunne@DESKTOP-QBDTI2D.localdomain>
2026-04-18 02:54:23 +0200

    [#128] Simplified and reduced redundant code

--- apps/frontend/app/(site)/layout.tsx
+++ apps/frontend/app/(site)/layout.tsx
@@ -1,24 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Providers from "@/components/Providers"; // Session Provider
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Ft_transcendence",
-  description: "A 42 project in collaboration of four 42 Wolfsburg students",
-};
 
 export default function SiteLayout({
                                      children,
@@ -26,12 +7,12 @@ export default function SiteLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+      <div>
         <Header />
         <main className="flex-grow">
           {children}
         </main>
         <Footer />
-      </body>
+      </div>
   );
 }
--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import Providers from "@/components/Providers"; // Session Provider
 
 const geistSans = Geist({
