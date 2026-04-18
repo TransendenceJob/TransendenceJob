a7c671325144324fd2b19796c6813aa2cca0ed18
Lilly K. Home <likunne@DESKTOP-QBDTI2D.localdomain>
2026-04-18 02:32:02 +0200

    [END] [#128] Finished

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
- 
+
 const nextConfig = {
     // Make the little Next Logo from the bottom left only appear in development mode
     devIndicators: (process.env.NODE_ENV == "development"),
